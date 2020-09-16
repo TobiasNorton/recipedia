014ad819706ce3b769fac88dc3861b27f32b1203
Tobias Norton <tnorton20@yahoo.com>
2020-09-16 15:52:06 -0400

    Set up for basic API calls for 50 recipes

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import { Formik, Form, Field } from 'formik'
+import axios from 'axios'
 import './style.scss'
 
 class Home extends Component {
@@ -8,20 +9,31 @@ class Home extends Component {
 
     this.state = {
       isAdvancedSearch: false,
+      recipes: [],
     }
   }
 
-  componentDidMount = () => {
-    console.log('HAHAHAHAHAHA componentDidMount()')
+  componentDidMount = () => {}
+
+  onSubmit = (values, { setStatus, setSubmitting, resetForm }) => {
+    console.log('onSubmit has definitely been clicked.', values)
+    const apiKey = '7115e309409d4387a6369108cd7185fd'
+    try {
+      this.search(values, apiKey)
+    } catch (error) {
+      setStatus({ error: error.message })
+    }
   }
 
-  simpleSearch = (values, { setStatus, setSubmitting, resetForm }) => {
-    console.log('onSubmit has definitely been clicked.', values)
-    // if (!this.state.isAdvancedSearch) {
-    //   try {
-    //     getRecipes(values)
-    //   }
-    // }
+  search = (values, apiKey) => {
+    // TODO: Create all of the query param strings to be concatenated in the url
+    console.log('search values', values)
+    const keyword = values && values.keyword
+    const recipesSearchUrl = `https://api.spoonacular.com/recipes/complexSearch?apiKey=${apiKey}&query=${keyword}&addRecipeInformation=true&number=50`
+    console.log('keywordddd', keyword)
+    axios
+      .get(recipesSearchUrl)
+      .then((response) => console.log('response', response && response.data && response.data))
   }
 
   setToAdvancedSearch = () => {
@@ -38,7 +50,7 @@ class Home extends Component {
         <h3>What are you hungry for?</h3>
 
         {!this.state.isAdvancedSearch ? (
-          <Formik initialValues={{ keyword: '' }} onSubmit={this.simpleSearch}>
+          <Formik initialValues={{ keyword: '' }} onSubmit={this.onSubmit}>
             {({ status, isSubmitting }) => {
               return (
                 <Form>
