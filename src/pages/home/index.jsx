6a242698f75d76819281d06d2d3218a5be8581eb
Tobias Norton <tnorton20@yahoo.com>
2020-11-25 20:37:30 -0500

    Add screens for loading and no results

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -8,7 +8,15 @@ import AdvancedSearchTest from '../../components/advanced-search-test'
 
 const Home = () => {
   const [isAdvancedSearch, setIsAdvancedSearch] = useState(false)
+  const [searchQuery, setSearchQuery] = useState('')
   const [recipes, setRecipes] = useState([])
+  const [couldNotFindRecipes, setCouldNotFindRecipes] = useState(false)
+
+  const refreshSearch = () => {
+    setSearchQuery('')
+    setRecipes([])
+    setCouldNotFindRecipes(false)
+  }
 
   const onSubmit = (values, { setStatus, setSubmitting, resetForm }) => {
     console.log('onSubmit has definitely been clicked.', values)
@@ -27,6 +35,7 @@ const Home = () => {
     const intolerancesQueryString = `&intolerances="${intolerances}"`
     const cuisines = values && values.cuisines && values.cuisines.join(',')
     const cuisineQueryString = `&cuisine="${cuisines}"`
+    setSearchQuery(keyword)
 
     let recipesSearchUrl = `https://api.spoonacular.com/recipes/complexSearch?apiKey=${apiKey}&query=${keyword}&addRecipeInformation=true&number=100`
     console.log('keyword: ', keyword)
@@ -43,6 +52,9 @@ const Home = () => {
     axios.get(recipesSearchUrl).then((response) => {
       console.log('response.data', response && response.data && response.data)
       setRecipes(response.data && response.data.results)
+      if (response.data && response.data.totalResults === 0) {
+        setCouldNotFindRecipes(true)
+      }
     })
   }
 
@@ -54,50 +66,76 @@ const Home = () => {
     setIsAdvancedSearch(false)
     setRecipes([])
   }
-  // Have a form for advanced vs simple
-  // If its a simple search, show the buttons, else show the whole form
 
   return (
     <div className="home">
-      <h3>What are you hungry for?</h3>
-      <div className="search-form-container">
-        {!isAdvancedSearch ? (
-          <Formik initialValues={{ keyword: '' }} onSubmit={onSubmit}>
-            {({ status, isSubmitting }) => {
-              return (
-                <Form>
-                  <Field className="text-field" type="text" name="keyword"></Field>
-                  <button className="search-button" type="submit" disabled={isSubmitting}>
-                    Search
-                </button>
-                  <button className="search-button" onClick={() => setToAdvancedSearch()}>
-                    Advanced
-                </button>
-                  {status && status.error ? <p>{status.error}</p> : null}
-                </Form>
-              )
-            }}
-          </Formik>
+      {
+        !searchQuery ? (
+          <div className="search-form-container">
+            <h3>What are you hungry for?</h3>
+            {!isAdvancedSearch ? (
+              <Formik initialValues={{ keyword: '' }} onSubmit={onSubmit}>
+                {({ status, isSubmitting }) => {
+                  return (
+                    <Form>
+                      <Field className="text-field" type="text" name="keyword"></Field>
+                      <button className="search-button" type="submit" disabled={isSubmitting}>
+                        Search
+                      </button>
+                      <button className="search-button" onClick={() => setToAdvancedSearch()}>
+                        Advanced
+                      </button>
+                      {status && status.error ? <p>{status.error}</p> : null}
+                    </Form>
+                  )
+                }}
+              </Formik>
+            ) : (
+                <AdvancedSearchTest onSubmit={onSubmit} backToSimpleSearch={backToSimpleSearch} />
+              )}
+          </div>
         ) : (
-            <AdvancedSearchTest onSubmit={onSubmit} backToSimpleSearch={backToSimpleSearch} />
-          )}
-      </div>
-      <div className="recipe-results">
-        {recipes.length
-          ? recipes.map((recipe, index) => {
-            const summary = `<p>${recipe.summary}</p>`
-            return (
-              <RecipeSnippet
-                id={recipe.id}
-                key={`recipe-${index}-${recipe.id}`}
-                summary={summary}
-                title={recipe.title}
-                image={recipe.image}
-              />
-            )
-          })
-          : null}
-      </div>
+            <div className="recipe-results-container">
+              {
+                recipes.length > 0 ? (
+                  <div>
+                    <h3>{`Showing ${recipes.length} results for "${searchQuery}"`}</h3>
+                    <button onClick={() => refreshSearch()}>Back to Search</button>
+                    <div className="recipe-results">
+                      {recipes.map((recipe, index) => {
+                        const summary = `<p>${recipe.summary}</p>`
+                        return (
+                          <RecipeSnippet
+                            id={recipe.id}
+                            key={`recipe-${index}-${recipe.id}`}
+                            summary={summary}
+                            title={recipe.title}
+                            image={recipe.image}
+                          />
+                        )
+                      })}
+                    </div>
+                    <button onClick={() => refreshSearch()}>Back to Search</button>
+                  </div>
+                ) : (
+                    <div>
+                      {
+                        couldNotFindRecipes ? (
+                          <div>
+                            <h3>{`Hmmm, we're not finding anything for "${searchQuery}".`}</h3>
+                            <p>Showing 0 results.</p>
+                            <button onClick={() => refreshSearch()}>Back to Search</button>
+                          </div>
+                        ) : (
+                            <div>Loading...</div>
+                          )
+                      }
+                    </div>
+                  )
+              }
+            </div>
+          )
+      }
 
       <p>
         I created this using the{' '}
