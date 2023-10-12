18c15b7997c0cdf98a8e2461f811d89b9f9fdcdf
Tobias Norton <tnorton20@yahoo.com>
2023-10-11 23:50:55 -0400

    Add font awesome as dependency, use FontAwesomeIcon component

--- src/components/navigation-bar/index.jsx
+++ src/components/navigation-bar/index.jsx
@@ -1,12 +1,14 @@
 import './style.scss';
 import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPepperHot } from '@fortawesome/free-solid-svg-icons';
 
 const NavigationBar = () => {
   return (
     <div className="navigation-bar">
       <Link to="/" className="logo">
         Recipedia
-        <i className="fa-solid fa-pepper-hot" />
+        <FontAwesomeIcon icon={faPepperHot} />
       </Link>
     </div>
   );
